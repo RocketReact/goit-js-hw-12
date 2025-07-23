f82041a2b317b00f372c701a4d1bb9944fe19c95
Oleg Shapoval <olegkazhan@gmail.com>
2025-07-23 09:21:57 +0300

    fix btn width Load more

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { hideLoader } from './render-functions.js';
 
 //TODO get array img axios API pixabay
 export default async function getImagesByQuery(query, page) {
